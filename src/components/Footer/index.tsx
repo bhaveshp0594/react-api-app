a7517cf1af0956204727da700631ba6120e4f18a
unknown <ac00493930@techmahindra.com>
2022-09-28 20:16:05 +0530

    git api integration using react app

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+import { FaHeart } from 'react-icons/fa';
+
+export function Footer() {
+    return (
+        <Container>
+        
+        </Container>
+    );
+}
